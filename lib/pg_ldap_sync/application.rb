76ae398afe9c995b847a47044f89d1b59adb516c
Toni Mas <antoni.mas@gmail.com>
2018-01-31 09:40:18 +0100

    Fix typo error

--- lib/pg_ldap_sync/application.rb
+++ lib/pg_ldap_sync/application.rb
@@ -152,7 +152,7 @@ class Application
     groups = []
     res = pg_exec "SELECT rolname, oid FROM pg_roles WHERE #{pg_groups_conf[:filter]}"
     res.each do |tuple|
-      res2 = pg_exec "SELECT pr.rolname FROM pg_auth_members pam JOIN pg_roles pr ON pr.oid=pam.member WHERE pam.roleid=#{PG:Connection.escape(tuple[1])}"
+      res2 = pg_exec "SELECT pr.rolname FROM pg_auth_members pam JOIN pg_roles pr ON pr.oid=pam.member WHERE pam.roleid=#{PG::Connection.escape(tuple[1])}"
       member_names = res2.map{|row| row[0] }
       group = PgRole.new tuple[0], member_names
       log.info{ "found pg-group: #{group.name.inspect} with members: #{member_names.inspect}"}
@@ -320,7 +320,7 @@ class Application
     ldap_groups = uniq_names search_ldap_groups
 
     # gather PGs users and groups
-    @pgconn = PG:connection.open @config[:pg_connection]
+    @pgconn = PG::Connection.open @config[:pg_connection]
     pg_users = uniq_names search_pg_users
     pg_groups = uniq_names search_pg_groups
 
